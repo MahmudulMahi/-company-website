90dfb4306a08545184944fb8fd398ee12b5fe0b9
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-11-02 02:30:47 +0600

    create proud of jurney section

--- src/components/Layouts/ProudJourney/ProudJourney.jsx
+++ src/components/Layouts/ProudJourney/ProudJourney.jsx
@@ -1,20 +1,20 @@
-import proudicon1 from '../../../../public/asset/Rating.png'
-import proudicon2 from '../../../../public/asset/Global Businessman.png'
-import proudicon3 from '../../../../public/asset/Vector.png'
-import proudicon4 from '../../../../public/asset/Time Zone.png'
-import proudicon5 from '../../../../public/asset/Vector (1).png'
+import proudicon1 from '/asset/Rating.png'
+import proudicon2 from '/asset/Global Businessman.png'
+import proudicon3 from '/asset/Vector.png'
+import proudicon4 from '/asset/Time Zone.png'
+import proudicon5 from '/asset/Vector (1).png'
 
 const ProudJourney = () => {
   return (
     <div>
       <div className='text-[#4169E1] text-3xl text-center font-semibold'>
-        ~We Are Proud of Our Journey~
+        <span className='shadow-2xl rounded-full p-0 md:p-3'>~We Are Proud of Our Journey~</span>
       </div>
 
-      <div className='grid grid-cols-5 gap-4'>
+      <div className='flex flex-col md:flex-col lg:flex-row justify-between gap-4 mt-10 mx-auto'>
         {/* ---------- */}
         <div className="card  bg-base-100 shadow-xl">
-          <div className="card-body items-center text-center">
+          <div className="card-body items-center text-center px-12">
             <img src={proudicon1} alt="" />
             <p className='text-3xl text-[#4169E1] font-bold'>2+</p>
             <p className='text-[24px]'>Years of  <br/>
@@ -25,7 +25,7 @@ const ProudJourney = () => {
         {/* ----------- */}
         {/* ---------- */}
         <div className="card  bg-base-100 shadow-xl">
-          <div className="card-body items-center text-center">
+          <div className="card-body items-center text-center px-8">
             <img src={proudicon2} alt="" />
             <p className='text-3xl text-[#4169E1] font-bold'>2.5k+</p>
             <p className='text-[24px]'>Of customers <br/>
@@ -35,8 +35,8 @@ const ProudJourney = () => {
         </div>
         {/* ----------- */}
         {/* ---------- */}
-        <div className="card  bg-base-100 shadow-xl">
-          <div className="card-body items-center text-center">
+        <div className="card    bg-base-100 shadow-xl">
+          <div className="card-body items-center text-center px-12">
             <img src={proudicon3} alt="" />
             <p className='text-3xl text-[#4169E1] font-bold'>2.5k+</p>
             <p className='text-[24px]'>Projects <br/>
@@ -46,8 +46,8 @@ const ProudJourney = () => {
         </div>
         {/* ----------- */}
         {/* ---------- */}
-        <div className="card  bg-base-100 shadow-xl">
-          <div className="card-body items-center text-center">
+        <div className="card   bg-base-100 shadow-xl">
+          <div className="card-body items-center text-center px-12">
             <img src={proudicon4} alt="" />
             <p className='text-3xl text-[#4169E1] font-bold'>13</p>
             <p className='text-[24px]'>Time zone <br/>
@@ -57,8 +57,8 @@ const ProudJourney = () => {
         </div>
         {/* ----------- */}
         {/* ---------- */}
-        <div className="card  bg-base-100 shadow-xl">
-          <div className="card-body items-center text-center">
+        <div className="card   bg-base-100 shadow-xl">
+          <div className="card-body items-center text-center px-12">
             <img src={proudicon5} alt="" />
             <p className='text-3xl text-[#4169E1] font-bold'>4.9/5</p>
             <p className='text-[24px]'>Customer <br/>
