2beb5c3244479fb092dad605fa9f504838b9b936
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-11-06 01:56:31 +0600

    create contact section

--- src/components/Layouts/contact/Contact.jsx
+++ src/components/Layouts/contact/Contact.jsx
@@ -1,9 +1,12 @@
 import bgimg from '/asset/Rectangle 297.png'
-// import bgimagerec1 from '../../../../public/asset/iconoir_agile.png'
+import bgimagerec1 from '../../../../public/asset/dashicons_email-alt.png'
+import icon1 from '/asset/mingcute_location-3-line.png'
+import icon2 from '/asset/gg_phone.png'
+import icon3 from '/asset/dashicons_email-alt.png'
 const Contact = () => {
   return (
-  
-    <div className='mt-12 relative' style={{backgroundImage: `url('/asset/Rectangle 297.png')`, backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '50vh'}} >
+
+    <div className='mt-12 relative' style={{ backgroundImage: `url('/asset/Rectangle 297.png')`, backgroundSize: 'cover', backgroundPosition: 'center', minHeight: '50vh' }} >
 
       <div className='flex flex-col md:flex-row justify-between z-20 p-7 '>
         <div>
@@ -11,9 +14,41 @@ const Contact = () => {
 
           <h2 className='text-3xl font-bold mt-10'>You can <span className='text-[#4169E1]'>connect</span> with us <br />
             when need <span className='text-[#4169E1]'>help!</span></h2>
+          <div className='mt-8 space-y-6 '>
+            <div className='flex space-x-4 items-center '>
+              <div className='bg-white p-5 rounded-xl border' >
+                <img src={icon1} alt="" />
+              </div>
+              <div className=''>
+                <p className=''>Office Location</p>
+                <p>Mirpur-14, Dhaka-1230, </p>
+                <p>Bangladesh</p>
+              </div>
+            </div>
+            <div className='flex space-x-4 items-center'>
+              <div className='bg-white p-5 rounded-xl border'>
+                <img src={icon2} alt="" />
+              </div>
+              <div>
+                <p>Phone</p>
+                <p>017XX-XXXXXXX</p>
+                <p>019XX-XXXXXXX</p>
+              </div>
+            </div>
+            <div className='flex space-x-4 items-center'>
+              <div className='bg-white p-5 rounded-xl border'>
+                <img src={icon3} alt="" />
+              </div>
+              <div>
+                <p>Email</p>
+                <p>sajibsazzadulalam@gmail.com </p>
+                <p>sajibsazzadulalam@gmail.com</p>
+              </div>
+            </div>
+          </div>
         </div>
         <div>
-          <form className="card-body border md:w-[600px] ">
+          <form className="card-body border md:w-[600px] mt-10">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -41,7 +76,7 @@ const Contact = () => {
           </form>
         </div>
       </div>
-    
+
 
     </div>
 
