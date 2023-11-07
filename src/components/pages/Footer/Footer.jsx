9be3d287dab087ce380fce9ce2e4555f9c4e10af
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-11-07 15:05:29 +0600

    create footer section

--- src/components/pages/Footer/Footer.jsx
+++ src/components/pages/Footer/Footer.jsx
@@ -1,25 +1,35 @@
 import icon1 from '../../../../public/asset/Group 393.png'
+import socalicon1 from '../../../../public/asset/logos_facebook.png'
+import socalicon2 from '../../../../public/asset/skill-icons_linkedin.png'
+import socalicon3 from '../../../../public/asset/Instragram Icon.png'
+import socalicon4 from '../../../../public/asset/Twitter Icon.png'
 
 const Footer = () => {
   return (
 
-      <footer className=" p-10 bg-base-200 text-base-content mt-56 ">
+    <footer className=" p-10 bg-base-200 text-base-content mt-56 ">
 
-    <div className="footer mx-auto max-w-7xl">
+      <div className="footer mx-auto max-w-7xl">
 
-      <div>
-      <img className='w-[100px]' src={icon1} alt="" />
+        <div>
+          <img className='w-[100px]' src={icon1} alt="" />
 
-      <h2 className='font-medium text-3xl'>Visionary</h2>
-      <p className='text-xl'>Tech Solutions</p>
+          <h2 className='font-medium text-3xl'>Visionary</h2>
+          <p className='text-xl'>Tech Solutions</p>
 
-      <p className='w-60'>Assurance yet bed was improving furniture man. Distrusts delighted Excuse few the remain highly feebly add people manner say. It high at my mind by roof. </p>
+          <p className='w-60'>Assurance yet bed was improving furniture man. Distrusts delighted Excuse few the remain highly feebly add people manner say. It high at my mind by roof. </p>
 
-      <div>
-
-      </div>
-      </div>
-    <nav>
+          <div>
+              <h2 className='text-2xl mb-4'>Find Us on social media</h2>
+              <div className='flex gap-4'>
+              <img src={socalicon1} alt="" />
+              <img src={socalicon2} alt="" />
+              <img src={socalicon3} alt="" />
+              <img src={socalicon4} alt="" />
+              </div>
+          </div>
+        </div>
+        <nav>
           <header className="footer-title">Services</header>
           <a className="link link-hover">Branding</a>
           <a className="link link-hover">Design</a>
@@ -39,9 +49,9 @@ const Footer = () => {
           <a className="link link-hover">Privacy policy</a>
           <a className="link link-hover">Cookie policy</a>
         </nav>
-    </div>
-  
-      </footer >
+      </div>
+
+    </footer >
 
   );
 };
