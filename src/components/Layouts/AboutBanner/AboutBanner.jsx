33eed5adde3b81633ed83983867d2320d6b05975
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-11-12 11:55:28 +0600

    create about banner  section

--- src/components/Layouts/AboutBanner/AboutBanner.jsx
+++ src/components/Layouts/AboutBanner/AboutBanner.jsx
@@ -4,18 +4,20 @@ import underimg from "/public/asset/Rectangle 344.png"
 const AboutBanner = () => {
   return (
     <>
-      <div className="hero  h-[300px] md:h-[500px] mt-3" 
+      <div className="hero    h-[300px] md:h-[500px] mt-3" 
       
       style={{ backgroundImage: "url('/asset/Rectangle 387.png')" 
       
       }}>
-        <div className="hero-overlay bg-opacity-10 bg-gradient-to-r from-blue-500  to-transparent h-full w-full "></div>
-        <div className="hero-content pl-8 absolute items-start text-neutral-content left-1 md:left-36 top- md:top-72">
+        <div className="hero-overlay bg-opacity-10 bg-gradient-to-r from-blue-500  to-transparent  "></div>
+  
+      <div className=" flex items-center justify-start text-neutral-content  mr-auto ml-28 ">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">ABOUT US</h1>
+            <h1 className=" text-5xl  font-bold">ABOUT US</h1>
             <p className="mb-5">We partner with you to fulfil your requirements for on-demand talent and a flexible workforce</p>        
           </div>
         </div>
+      
       </div>
       <img src={underimg} alt="" />
     </>
