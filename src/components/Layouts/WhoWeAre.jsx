5cdd770807511d9beed5be890a3ce57262762408
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-11-13 14:35:42 +0600

    create about our team section

--- src/components/Layouts/WhoWeAre.jsx
+++ src/components/Layouts/WhoWeAre.jsx
@@ -6,60 +6,67 @@ const WhoWeAre = () => {
   return (
     <div className='mt-20'>
 
-      
-        <div className='flex flex-col md:flex-row gap-5 justify-between  '>
 
-          <div className='w-[380px] md:w-[500px]'>
-            <h2 className="text-3xl text-[#4169E1] mt-4 font-medium mb-5"> <span className='shadow-2xl rounded-full p-3'>~Who We Are~</span></h2>
-            <span className='block mb-3'>
-              Since 2022 Visionary Tech Solution has national presence, offering Information Technologies solutions, committed with the innovation, quality and any adjustment for the company´s requirements, our culture is set by values that contributes to create a flexible, creative, implication related and teamwork.
-            </span>
+      <div className='flex flex-col md:flex-row gap-20   '>
 
-            <span className='block mb-3'>
-              We believe that our customers’ success is our own success, this is why we stablish strategic alliances with our customers in their IT projects development in Bangladesh.
-            </span>
+        <div className='flex-1'>
+          <h2 className="text-3xl text-[#4169E1] mt-4 font-medium mb-14"> <span className='shadow-2xl rounded-full p-3'>~Who We Are~</span></h2>
+          <span className='block mb-3'>
+            Since 2022 Visionary Tech Solution has national presence, offering Information Technologies solutions, committed with the innovation, quality and any adjustment for the company´s requirements, our culture is set by values that contributes to create a flexible, creative, implication related and teamwork.
+          </span>
 
-            <span>
-              We have a wide experience at technical level, a young professional team, with a solid academic formation and advances technical skills who let us deliver high quality solutions to any kind of company
-            </span>
-          </div>
-          <div className=''>
-            <img className='w-[600px] h-[400px] rounded-lg' src={whoareimg} alt="" />
-          </div>
+          <span className='block mb-3'>
+            We believe that our customers’ success is our own success, this is why we stablish strategic alliances with our customers in their IT projects development in Bangladesh.
+          </span>
+
+          <span>
+            We have a wide experience at technical level, a young professional team, with a solid academic formation and advances technical skills who let us deliver high quality solutions to any kind of company
+          </span>
         </div>
-        <div className='flex gap-3 mt-9'>
+        <div className=' flex-1'>
+          <img className='w-[600px] h-[400px] rounded-lg' src={whoareimg} alt="" />
+        </div>
+      </div>
+      <div className='flex gap-16 mt-14'>
         {/* ------------- */}
-          <div className='w-1/3'>
-            <div className='flex flex-col items-center'>
-              <img  src={iconAbout1} alt="" />
-              <h2 className='text-[#4169E1] font-medium'>Out Vision</h2>
-            </div>
-            Our vision is focused to be a leader company, model at solutions and Information Technology development, solid, profitable and always evolving.
+      
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure className="px-10 pt-5">
+          <img  src={iconAbout1} alt="" />
+          </figure>
+          <div className="card-body pt-5 items-center text-center">
+          <h2 className='text-[#4169E1] font-medium'>Out Vision</h2>
+            <p>Our vision is focused to be a leader company, model at solutions and Information Technology development, solid, profitable and always evolving.</p>
           </div>
-          {/* ---------- */}
-          {/* ---------- */}
-          <div className='w-1/3'>
-            <div className='flex flex-col items-center'>
-              <img src={iconAbout2} alt="" />
-              <h2 className='text-[#4169E1] font-medium'>Our Mission</h2>
-            </div>
-            Our Mission is based on the productive growth of our customers, delivering tools, knowledge and Information Technology to satisfy their requirements promoting a harmonious development for our human talent.
-          </div>
-          {/* ---------- */}
-               {/* ---------- */}
-            <div className='w-1/3'>
-            <div className='flex flex-col items-center'>
-              <img src={iconAbout3} alt="" />
-              <h2 className='text-[#4169E1] font-medium'>Our Values</h2>
-            </div>
-            Our business values are based on a clear services principle, quality, honesty and commitment. Our corporative principles are closely related to the continuous improvement, competitiveness, teamwork, human talent commitment and the democratic participation of our human resources.
-          </div>
-          {/* ---------- */}
-          
         </div>
+        {/* ---------- */}
+        {/* ---------- */}
+        <div className="card w-96  bg-base-100 shadow-xl">
+          <figure className="px-10 pt-5">
+          <img  src={iconAbout2} alt="" />
+          </figure>
+          <div className="card-body pt-5 items-center text-center">
+          <h2 className='text-[#4169E1] font-medium'>Our Mission</h2>
+            <p>Our Mission is based on the productive growth of our customers, delivering tools, knowledge and Information Technology to satisfy their requirements promoting a harmonious development for our human talent.</p>
+          </div>
+        </div>
+        {/* ---------- */}
+        {/* ---------- */}
+          <div className="card w-96 bg-base-100 shadow-xl">
+          <figure className="px-10 pt-5">
+          <img  src={iconAbout3} alt="" />
+          </figure>
+          <div className="card-body pt-5 items-center text-center">
+          <h2 className='text-[#4169E1] font-medium'>Our Values</h2>
+            <p>Our business values are based on a clear services principle, quality, honesty and commitment. Our corporative principles are closely related to the continuous improvement, competitiveness, teamwork, human talent commitment and the democratic participation of our human resources.</p>
+          </div>
+        </div>
+        {/* ---------- */}
 
       </div>
-  
+
+    </div>
+
   );
 };
 
