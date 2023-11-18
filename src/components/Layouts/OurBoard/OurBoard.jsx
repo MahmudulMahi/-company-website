3f585c1b935b6e6316f5ecb5db60af0610a452d8
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2023-11-18 21:23:57 +0600

    create our borad section

--- src/components/Layouts/OurBoard/OurBoard.jsx
+++ src/components/Layouts/OurBoard/OurBoard.jsx
@@ -1,27 +1,27 @@
+import { useEffect, useState } from 'react';
 import profail from '../../../../public/asset/mahmudul.jpg'
+import Ourbordcard from './Ourbordcard/Ourbordcard';
 
 const OurBoard = () => {
+
+  const [profail, setprofail]=useState([])
+
+  useEffect(()=>{
+    fetch('/profail.json')
+    .then(res =>res.json())
+    .then(data =>setprofail(data))
+  },[])
   return (
-    <div className="mt-60">
+    <div className="mt-14">
       <h2 className='text-[#4169E1] text-3xl text-center font-semibold mt-10 mb-8'><span className='shadow-2xl rounded-full p-0 md:p-3'>~ Our Board ~</span></h2>
 
-      <div>
-        <div className="card card-compact w-96 h-96  bg-base-100 shadow-xl bg-cover relative " style={{
-        
-        backgroundImage: "url('/asset/profailpic.png')", backgroundSize: "320px",
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: ""
-      }}>
-          <div>
-          <figure><img className='w-52 h-56 rounded-full absolute top-8 left-[78px] ' src={profail} alt="Shoes" /></figure>
-          </div>
-          <div className="card-body absolute bottom-0">
-            <h2 className="card-title">Ashraful Islam Spondon</h2>
-            <p>Chief Executive Officer</p>
-         
-          </div>
-        </div>
+      <div className='grid grid-cols-1 md:grid-cols-2 justify-items-center gap-8'>
+        {
+          profail.map(pro => <Ourbordcard key={pro.id} className='flex-1 max-w-[calc(50%-1rem)] m-2' pro={pro}></Ourbordcard>)
+        }
       </div>
+
+   
     </div>
   );
 };
